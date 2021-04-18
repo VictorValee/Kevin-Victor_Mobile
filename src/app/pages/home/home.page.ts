019a512d8771b2fa0738a697b6b05a3569bd4981
Kevin Lokoka <68908952+KevinL45@users.noreply.github.com>
2021-04-18 11:00:19 +0200

    appli

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -41,18 +41,39 @@ export class HomePage implements OnInit {
                 category : "Oeuf",
                 createAt : new Date(),
                 averageStar : 4, 
-                picture : [
-                    'assets/img/recettes/oeufs/oeuf1.jpg'
-                ]}
+                picture : 'assets/img/recettes/oeuf1.jpg'}
                 ,{
                 title : "Poêlée d'épinards frais au beurre",
-                description :"Découvrez notre recette facile et rapide de Poêlée d'épinards frais au beurre",
+                description :"Découvrez notre recette facile et rapide de poêlée d'épinards frais au beurre",
                 category : "Recette traditionnelle",
                 createAt : new Date(),
                 averageStar : 5, 
-                picture : [
-                    'assets/img/recettes/rts/rt1.jpg'
-                ]}
+                picture : 'assets/img/recettes/rt1.jpg'
+                }
+                 ,{
+                title : "Poulet Yassa",
+                description :"Le yassa est un plat sénégalais à base d'oignons frits et de riz et qui peut être accompagné de viande marinée dans le citron puis frite ou braisée, de poulet ou de poisson. ",
+                category : "Poulet",
+                createAt : new Date(),
+                averageStar : 3, 
+                picture : 'assets/img/recettes/yassa.jpg'
+                }
+                 ,{
+                title : "Crème au chocolat light",
+                description :"Une recette de crème au chocolat maison, bien meilleur et saine que celles des grandes surfaces.",
+                category : "Crème",
+                createAt : new Date(),
+                averageStar : 2, 
+                picture : 'assets/img/recettes/chocolat.jpg'
+                }
+                 ,{
+                title : "Potatoes maison aux épices",
+                description :"Découvrez notre recette facile et rapide de potatoes maison aux épice",
+                category : "Pomme de terre",
+                createAt : new Date(),
+                averageStar : 5, 
+                picture : 'assets/img/recettes/patatoes.jpg'
+                }
             ]}
 
             
