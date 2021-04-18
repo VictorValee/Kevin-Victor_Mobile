1d1cc7d3d75be995226c10a8673df6e9c7b7a875
Kevin Lokoka <68908952+KevinL45@users.noreply.github.com>
2021-04-18 09:56:37 +0200

    appli

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -60,9 +60,9 @@ export class HomePage implements OnInit {
             //Montre les détails de la recette
             showDetails(recette){
                 console.log(recette)
-                this.router.navigate(['details',
+                this.router.navigate(['/details'],
                 {queryParams:recette},
-            ]);
+            );
             }
 
 
