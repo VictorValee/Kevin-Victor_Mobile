ac29b988c25ee3e713b5e72fce2460096bb742d0
Kevin Lokoka <68908952+KevinL45@users.noreply.github.com>
2021-04-26 15:54:43 +0200

    Firebase

--- src/app/pages/details/details.page.ts
+++ src/app/pages/details/details.page.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute, NavigationEnd, NavigationExtras } from '@angula
 import { NativeStorage } from '@ionic-native/native-storage/ngx';
 import { FavorisRecette } from '../../interfaces/favoris-recette';
 import { element } from 'protractor';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-details',
@@ -13,53 +14,20 @@ import { element } from 'protractor';
 export class DetailsPage implements OnInit {
 
 details:any=[];
+userId="";
 
-  constructor(public route:ActivatedRoute, private storage:NativeStorage) {
+  constructor(public route:ActivatedRoute, 
+    private storage:NativeStorage,
+    public AfAuth:AngularFireAuth
+    ){
     this.route.queryParams.subscribe((data)=>{
       console.log(data);
       this.details = data;
-
     })
-   }
-
+    }
    //Ajoute la recette dans les favoris
    addToFavoris(details : any){
-     //Creation de la variable
-     let ajouter : boolean = false;
-     this.storage.getItem("uneRecette").then((data:FavorisRecette[])=>{
-            
-      //Si il y a rien dans les favoris
-      if(data === null || data.length == 0){
         
-      //Initialisation du tableau data
-        data = [];
-        
-      //Ajoute dans les favoris
-      data.push({
-        item : details
-      })
-
-      }else{// Sinon
-
-        //Parcourir les favoris
-        for (let i = 0; i<data.length; i++){
-          const element : FavorisRecette = data[i];
-          if(details.id===element.item.id){
-            //Si les favoris n'est pas vide et contien la recette
-            ajouter=true;
-          }
-        }
-        if(!ajouter){
-          data.push({
-            item : details
-          })
-        }
-
-      }
-      this.storage.setItem("uneRecette",data);
-    });
-     
-     
 
    }
 
