8391aa36701aa1b3be9538aa084a9005f4bf2f40
Kevin Lokoka <68908952+KevinL45@users.noreply.github.com>
2021-08-12 19:36:18 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,13 +19,7 @@ import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFirestoreModule } from '@angular/fire/firestore/';
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyD0pkZ4Ghkca2e6kBGwhvouugdCXCK1hes",
-    authDomain: "foodbook-3da49.firebaseapp.com",
-    projectId: "foodbook-3da49",
-    storageBucket: "foodbook-3da49.appspot.com",
-    messagingSenderId: "219397367084",
-    appId: "1:219397367084:web:8e439b8eff543fc274f889",
-    measurementId: "G-7319FKWB13"
+
 };
 
 @NgModule({
